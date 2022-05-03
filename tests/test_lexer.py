2ed4b9c4b4c3d5684188dc08992799c36845b021
Andres Morales <andres.morales@ceiba.com.co>
2022-05-03 15:17:48 -0500

    Inicio ddel lexer y los tokens

---
+++ tests/test_lexer.py
@@ -0,0 +1,24 @@
+from typing import List
+from unittest import TestCase
+
+from lpp.lexer import Lexer
+from lpp.token import Token, TokenType
+
+
+class LexerTest(TestCase):
+
+  def test_ilegal(self) -> None:
+    source: str = '¡¿@'
+    lexer: Lexer = Lexer(source)
+
+    tokens: List[Token] = []
+    for i in range(len(source)):
+      tokens.append(lexer.next_token())
+
+    expected_tokens: List[Token] = [
+      Token(TokenType.ILLEGAL, '¡'),
+      Token(TokenType.ILLEGAL, '¿'),
+      Token(TokenType.ILLEGAL, '@')
+    ]
+
+    self.assertEquals(tokens, expected_tokens)
