4b4e171031e2a5a66d00d27f5bcbfe4ab8865e36
Andres Morales <andres.morales@ceiba.com.co>
2022-05-04 10:24:02 -0500

    Se agregan tokens y palagras reservadas

--- lpp/utils/const.py
+++ lpp/utils/const.py
@@ -4,17 +4,30 @@ from lpp.utils.type import TokenType
 
 
 TOKENS: Dict[str, TokenType] = {
-  '=': TokenType.ASSIGN,
-  '+': TokenType.PLUS,
-  '': TokenType.EOF,
-  '(': TokenType.LPAREN,
-  ')': TokenType.RPAREN,
-  '{': TokenType.LBRACE,
-  '}': TokenType.RBRACE,
-  ',': TokenType.COMMA,
-  ';': TokenType.SEMICOLON
+    '=': TokenType.ASSIGN,
+    '+': TokenType.PLUS,
+    '-': TokenType.MINUS,
+    '*': TokenType.MULTIPLICATION,
+    '/': TokenType.DIVISION,
+    '^': TokenType.POWER,
+    '(': TokenType.LPAREN,
+    ')': TokenType.RPAREN,
+    '{': TokenType.LBRACE,
+    '}': TokenType.RBRACE,
+    ',': TokenType.COMMA,
+    ';': TokenType.SEMICOLON,
+    '<': TokenType.LT,
+    '>': TokenType.GT,
+    '': TokenType.EOF
 }
 
 KEIWORDS: Dict[str, TokenType] = {
-  'let': TokenType.LET
+    'def': TokenType.FUNCTION,
+    'else': TokenType.ELSE,
+    'false': TokenType.FALSE,
+    'if': TokenType.IF,
+    'let': TokenType.LET,
+    'mod': TokenType.MOD,
+    'return': TokenType.RETURN,
+    'true': TokenType.TRUE
 }
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -72,7 +72,7 @@ class LexerTest(TestCase):
     self.assertEquals(tokens, expected_tokens)
 
   def test_assignment(self) -> None:
-    source: str = 'let cinco = 5;'
+    source: str = 'let five = 5;'
     lexer: Lexer = Lexer(source)
 
     tokens: List[Token] = []
@@ -81,7 +81,7 @@ class LexerTest(TestCase):
 
     expected_tokens: List[Token] = [
         Token(TokenType.LET, 'let'),
-        Token(TokenType.IDENT, 'cinco'),
+        Token(TokenType.IDENT, 'five'),
         Token(TokenType.ASSIGN, '='),
         Token(TokenType.INT, '5'),
         Token(TokenType.SEMICOLON, ';'),
@@ -89,7 +89,7 @@ class LexerTest(TestCase):
     self.assertEquals(tokens, expected_tokens)
 
   def test_assignment_str(self) -> None:
-    source: str = 'let str_cinco = \'cinco\';'
+    source: str = 'let str_five = \'five\';'
     lexer: Lexer = Lexer(source)
 
     tokens: List[Token] = []
@@ -98,9 +98,98 @@ class LexerTest(TestCase):
 
     expected_tokens: List[Token] = [
         Token(TokenType.LET, 'let'),
-        Token(TokenType.IDENT, 'str_cinco'),
+        Token(TokenType.IDENT, 'str_five'),
         Token(TokenType.ASSIGN, '='),
-        Token(TokenType.STR, '\'cinco\''),
+        Token(TokenType.STR, '\'five\''),
         Token(TokenType.SEMICOLON, ';'),
+    ]
+    self.assertEquals(tokens, expected_tokens)
+
+  def test_function_declaration(self) -> None:
+    source: str = '''
+      let sum = def(x, y) {
+        x + y;
+      };
+    '''
+    lexer: Lexer = Lexer(source)
+
+    tokens: List[Token] = []
+    for i in range(16):
+        tokens.append(lexer.next_token())
+
+    expected_tokens: List[Token] = [
+        Token(TokenType.LET, 'let'),
+        Token(TokenType.IDENT, 'sum'),
+        Token(TokenType.ASSIGN, '='),
+        Token(TokenType.FUNCTION, 'def'),
+        Token(TokenType.LPAREN, '('),
+        Token(TokenType.IDENT, 'x'),
+        Token(TokenType.COMMA, ','),
+        Token(TokenType.IDENT, 'y'),
+        Token(TokenType.RPAREN, ')'),
+        Token(TokenType.LBRACE, '{'),
+        Token(TokenType.IDENT, 'x'),
+        Token(TokenType.PLUS, '+'),
+        Token(TokenType.IDENT, 'y'),
+        Token(TokenType.SEMICOLON, ';'),
+        Token(TokenType.RBRACE, '}'),
+        Token(TokenType.SEMICOLON, ';')
+    ]
+    self.assertEquals(tokens, expected_tokens)
+
+  def test_fuction_call(self) -> None:
+    source: str = 'let result = sum(two, three);'
+    lexer: Lexer = Lexer(source)
+
+    tokens: List[Token] = []
+    for i in range(10):
+        tokens.append(lexer.next_token())
+
+    expected_tokens: List[Token] = [
+        Token(TokenType.LET, 'let'),
+        Token(TokenType.IDENT, 'result'),
+        Token(TokenType.ASSIGN, '='),
+        Token(TokenType.IDENT, 'sum'),
+        Token(TokenType.LPAREN, '('),
+        Token(TokenType.IDENT, 'two'),
+        Token(TokenType.COMMA, ','),
+        Token(TokenType.IDENT, 'three'),
+        Token(TokenType.RPAREN, ')'),
+        Token(TokenType.SEMICOLON, ';'),
+    ]
+    self.assertEquals(tokens, expected_tokens)
+
+  def test_control_statement(self) -> None:
+    source: str = '''
+      if (5 < 10) {
+        return true;
+      } else {
+        return false;
+      }
+    '''
+    lexer: Lexer = Lexer(source)
+
+    tokens: List[Token] = []
+    for i in range(17):
+        tokens.append(lexer.next_token())
+
+    expected_tokens: List[Token] = [
+        Token(TokenType.IF, 'if'),
+        Token(TokenType.LPAREN, '('),
+        Token(TokenType.INT, '5'),
+        Token(TokenType.LT, '<'),
+        Token(TokenType.INT, '10'),
+        Token(TokenType.RPAREN, ')'),
+        Token(TokenType.LBRACE, '{'),
+        Token(TokenType.RETURN, 'return'),
+        Token(TokenType.TRUE, 'true'),
+        Token(TokenType.SEMICOLON, ';'),
+        Token(TokenType.RBRACE, '}'),
+        Token(TokenType.ELSE, 'else'),
+        Token(TokenType.LBRACE, '{'),
+        Token(TokenType.RETURN, 'return'),
+        Token(TokenType.FALSE, 'false'),
+        Token(TokenType.SEMICOLON, ';'),
+        Token(TokenType.RBRACE, '}')
     ]
     self.assertEquals(tokens, expected_tokens)
